e239f4bf8f0e825979346e13df93b1151880dbe9
xifxif <fifipgst@gmail.com>
2025-12-07 17:50:52 +0700

    Add Fix bank_sampah_id migration to avoid duplicate column

--- database/migrations/2025_11_29_184852_add_bank_sampah_id_to_users_table.php
+++ database/migrations/2025_11_29_184852_add_bank_sampah_id_to_users_table.php
@@ -9,12 +9,26 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    // public function up(): void
+    // {
+    //     Schema::table('users', function (Blueprint $table) {
+    //         $table->foreignId('bank_sampah_id')->nullable()->after('email')->constrained('bank_sampah')->onDelete('set null');
+    //     });
+    // }
+
     public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('bank_sampah_id')->nullable()->after('email')->constrained('bank_sampah')->onDelete('set null');
-        });
-    }
+{
+    Schema::table('users', function (Blueprint $table) {
+        if (!Schema::hasColumn('users', 'bank_sampah_id')) {
+            $table->foreignId('bank_sampah_id')
+                ->nullable()
+                ->after('email')
+                ->constrained('bank_sampah')
+                ->onDelete('set null');
+        }
+    });
+}
+
 
     /**
      * Reverse the migrations.
