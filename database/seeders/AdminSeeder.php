473ec8275f2d377e1ddce6ce6097827b8ce9262d
xifxif <fifipgst@gmail.com>
2025-12-08 14:21:00 +0700

    Fix AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -13,36 +13,34 @@ class AdminSeeder extends Seeder
     {
         $this->command->info('Starting AdminSeeder...');
 
-        // Pastikan ada Bank Sampah
         $bankSampah = BankSampah::first();
-        if (!$bankSampah) {
-            $this->command->info('Bank Sampah kosong, membuat default...');
-            $bankSampah = BankSampah::create([
-                'nama' => 'Bank Sampah Default',
-                'alamat' => 'Alamat Default',
-            ]);
-        }
+if (!$bankSampah) {
+    $bankSampah = BankSampah::create([
+        'nama' => 'Bank Sampah Default',
+        'alamat' => 'Alamat Default',
+    ]);
+}
 
-        // Create Super Admin
-        $admin = User::updateOrCreate(
-            ['email' => 'admin@dlh.go.id'],
-            [
-                'name' => 'Admin DLH',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-            ]
-        );
+// Super Admin
+$admin = User::updateOrCreate(
+    ['email' => 'admin@dlh.go.id'],
+    [
+        'name' => 'Admin DLH',
+        'password' => Hash::make('password'),
+        'email_verified_at' => now(),
+    ]
+);
 
-        // Create Bank Sampah Admin
-        $banksampahUser = User::updateOrCreate(
-            ['email' => 'pedulilingkungan@gmail.com'],
-            [
-                'name' => 'Admin Pengelola',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-                'bank_sampah_id' => $bankSampah->id, // HARUS ada
-            ]
-        );
+// Bank Sampah Admin
+$banksampahUser = User::updateOrCreate(
+    ['email' => 'pedulilingkungan@gmail.com'],
+    [
+        'name' => 'Admin Pengelola',
+        'password' => Hash::make('password'),
+        'email_verified_at' => now(),
+        'bank_sampah_id' => $bankSampah->id, // PASTIKAN ADA BANK SAMPAH
+    ]
+);
 
         // Assign roles
         $admin->syncRoles(['admin']);
